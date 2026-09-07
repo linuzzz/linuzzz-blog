10f1021dbb898b82d5829486007e2477b03c784a
linuzzz <pabrizzo@gmail.com>
2026-09-07 11:31:20 +0200

    Adding integration with Cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,11 @@ import { defineConfig } from 'astro/config';
 
 import mdx from '@astrojs/mdx';
 
+import cloudflare from '@astrojs/cloudflare';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [mdx()],
   image: { layout: 'constrained' },
+  adapter: cloudflare(),
 });
